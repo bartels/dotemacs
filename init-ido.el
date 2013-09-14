9028b4a6a9b9fbd2141d17ba3965dde4f5dcc4a9
Eric Bartels <ebartels@gmail.com>
2013-09-14 10:30:41 -0700

    Changed ido window size and num prospects

--- init-ido.el
+++ init-ido.el
@@ -3,6 +3,8 @@
 (setq ido-everywhere t)
 (setq ido-enable-flex-matching t)
 (setq ido-create-new-buffer 'always)
+(setq ido-max-window-height 0.4)
+(setq ido-max-prospects 40)
 
 (require-package 'ido-ubiquitous)
 (ido-ubiquitous-mode t)
