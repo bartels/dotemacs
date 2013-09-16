532b94e240d33f79d5281348a0802179900478cb
Eric Bartels <ebartels@gmail.com>
2013-09-16 13:42:27 -0700

    Some more extensive commenting for escreen code

--- init-escreen.el
+++ init-escreen.el
@@ -1,12 +1,29 @@
 (require-package 'escreen)
 (require 'escreen)
 
-;;; set escreen key prefix
+;;; init escreen
 (setq escreen-prefix-char "\C-a"
       escreen-max-screens 100)
 (escreen-install)
 
-;;; escreen screens "tabbar"
+
+;;;; window/screen management
+(defun escreen-close-window-or-screen ()
+  "Closes escreen screen if there is only one window in the screen, otherwise
+deletes the current window."
+  (interactive)
+  (if (one-window-p)
+      (escreen-kill-screen)
+    (delete-window)))
+
+
+;;; escreen "tabs"
+; escreen provides an interface similar to gnu screen where switching between
+; screens preserves your buffers and window configurations. But it does not
+; provide a good interface for seeing which screens are available, and which is
+; the active one.  The below code displays the active escreen screens, and shows
+; which screen is currently being viewed in a "tab-like" interface in the
+; message area at hte bottom of the screen.
 (defun escreen-get-number-and-name (config)
   "Returns (number . buffer-name) for the escreen config"
   (cons
@@ -37,14 +54,6 @@
   "Shows message \"tabbar\" containing escreen labels with active one highlighted"
   (interactive)
   (message (escreen-active-screen-names)))
-
-(defun escreen-close-window-or-screen ()
-  "Closes escreen screen if there is only one window, otherwise deletes the
-current window."
-  (interactive)
-  (if (one-window-p)
-      (escreen-kill-screen)
-    (delete-window)))
 
 (add-hook 'escreen-goto-screen-hook 'escreen-show-active-screen-names)
 (define-key escreen-map "l" 'escreen-show-active-screen-names)
