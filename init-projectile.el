3c0e228110872ecae76440f4421155dacf7fb114
Eric Bartels <ebartels@gmail.com>
2013-09-17 17:13:43 -0700

    Some changes to projectile setup
    - Now using 'find' across the board instead of vcs specific methods
    - Added some glob patterns for find to globally ignore

--- init-projectile.el
+++ init-projectile.el
@@ -1,19 +1,43 @@
 (require-package 'projectile)
 (require 'projectile)
 
-(setq projectile-indexing-method 'alien)
-(setq projectile-git-command "find . -type f \\( ! -name *.elc ! -name *.pyc \\) -print0")
-(setq projectile-require-project-root nil)
-(setq projectile-enable-caching t)
-(setq projectile-cache-file (concat user-cachedir "projectile.cache" ))
-(setq projectile-known-projects-file (concat user-cachedir "projectile-bookmarks.eld" ))
+(setq projectile-indexing-method 'alien
+      projectile-require-project-root nil
+      projectile-enable-caching t
+      projectile-cache-file (concat user-cachedir "projectile.cache" )
+      projectile-known-projects-file (concat user-cachedir "projectile-bookmarks.eld"))
 
 (setq projectile-globally-ignored-directories
       (append projectile-globally-ignored-directories
-              '("tmp" ".cache")))
+              '("tmp"
+                ".cache")))
+
+;;; projectile doesn't give a way to specify global patterns to ignore, so we
+;;; build the arguments to the file command here with the patterns we want to
+;;; ignore
+(setq projectile-ignore-patterns
+      '("*.elc"
+        "*.pyc"
+        "*.pyo"
+        "*.o"
+        "*~"))
+
+(setq projectile-generic-command
+      (format "find . -type f \\( %s \\) -print0"
+              (mapconcat (lambda (pattern) (format "! -name %s" pattern))
+                         projectile-ignore-patterns " ")))
+
 
 (after-load 'projectile
     (diminish 'projectile-mode))
+
+(require 'projectile)
+
+;; don't use vcs specific files (we want projectile to work on all files in the
+;; project directory)
+(defun projectile-project-vcs ()
+  "Determine the VCS used by the project if any."
+  'none)
 
 (projectile-global-mode)
 
