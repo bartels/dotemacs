5a7099a93547ea83f0c3a329c19c74fc81963092
Eric Bartels <ebartels@gmail.com>
2013-09-22 18:36:54 -0700

    Set window dimensions for new frames

--- init.el
+++ init.el
@@ -2,6 +2,7 @@
 (setq inhibit-startup-message t)
 (tool-bar-mode -1)
 (toggle-scroll-bar -1)
+(setq default-frame-alist '((width . 160) (height . 56)))
 
 (add-to-list 'load-path user-emacs-directory)
 (defvar user-cachedir
